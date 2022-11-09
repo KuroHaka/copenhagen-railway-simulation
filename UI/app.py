144f7bb227f0c414607b0862e7996ef53204140e
Fenfen <hdzwliu@qq.com>
2022-11-09 22:39:27 +0100

    flask test, a small demon
    1. I have add something into the UI based on the information in Linear.
    
    2.  I created another UI for test, that is GUI. I think these is a small problem in GUI\templates\login1.html.  but I don't know how to figure it out.
    
    3.  I don't know how to use flask to implement the data interaction between frontend and backend

--- UI/app.py
+++ UI/app.py
@@ -1,11 +1,32 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__)  #flask instance
 
-@app.route("/")
-def hello_there(name = None):
+@app.route("/")   #url address
+def hello_there(name = None):  # process functions
     return render_template(
         "index.html",
         name=name
     )
+
+@app.route('/')
+def index():
+    return {
+        "msg": "success",
+        "data": "welcome to use flask."
+    }
+
+
+def user_info(u_id):
+    return {
+        "msg": "success",
+        "data": {
+            "id": u_id,
+            "username": 'yuz',
+            "age": 18
+        }
+    }
+
+if __name__=='__main__':
+    app.run()
