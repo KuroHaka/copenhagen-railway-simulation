433c04f69d4ae32e0daa14a043377575ffda8e65
MichaelHuu <s220423@dtu.dk>
2022-11-10 21:09:31 +0100

    Update Passenger.py
    added how much time it takes for passenger to travel

--- train_simulation/Passenger.py
+++ train_simulation/Passenger.py
@@ -8,12 +8,15 @@ class Passenger:
     _id = ''
     #is travel time needed?
     _travelTimeEnd = datetime
+    _travelTime = datetime
+    _isArrived = bool
 
     def __init__(self,currentlocation,destination,departuetime):
         self._currentlocation = currentlocation
         self._destination = destination
         self._departureTime = departuetime
         self._id = uuid.uuid4()
+        self._isArrived = False
 
     def getdestination(self):
         return self._destination
@@ -42,11 +45,27 @@ class Passenger:
         print(f"current location: {self._currentlocation}")
         print(f"destination: {self._destination}")
         print(f"departuretime: {self._departureTime}")
+        if self._isArrived:
+            print(f"travel time: {self._travelTime}")
+
+    def isArrived(self):
+        if(self._departureTime==self._departureTime):  
+            self._travelTimeEnd = datetime.now()
+            self._travelTime = self._travelTimeEnd - self._departureTime  
+            self._isArrived = True
+
 
 
 #Testing delete this
+import time
 
 p1 = Passenger('Lyngby','kbh h',datetime.now())
 print('test')
-p1.printInformation();
+p1.printInformation()
+
+p1.setlocation('kbh h')
+time.sleep(3)
+p1.isArrived()
+p1.printInformation()
+
 
