431d95eb235e7057f90dea9cc9f711004bc0f8ea
nirbelelti <nirbelelti@gmail.com>
2022-11-11 09:29:10 +0100

    Person class and statistic class

--- train_simulation/Person.py
+++ train_simulation/Person.py
@@ -1,3 +1,29 @@
+from others.simulation_skeleton import Person
+from datetime import datetime
+
 
 class Person:
-    pass
+    def __init__(self, start_station, destination, traffic_load):
+        self.start_station = start_station
+        self.destination = destination
+        self.traffic_load = traffic_load
+        self.status = 'ready'
+        self.start_time = datetime.now()
+        self.end_time = None
+        self.travel_time = None
+
+    def start_ride(self):
+        self.status = 'on board'
+
+    def end_ride(self):
+        self.status = 'done'
+        self.end_time = datetime.now()
+        self.travel_time = self.ride_duration()
+        #TODO cal method to register ride
+        self.delete_person()
+
+    def delete_person(self):
+        del self
+
+    def ride_duration(self):
+        return self.end_time -self.start_time
