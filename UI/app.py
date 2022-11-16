233ece6b0e0f2e9b31412bec244e8f09cf6fa705
Gene Chen <ceschen27@gmail.com>
2022-11-16 04:41:31 +0100

    add import

--- UI/app.py
+++ UI/app.py
@@ -1,6 +1,12 @@
 # importing Flask and other modules
 from flask import Flask, request, render_template
 
+import json, os, sys
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.append(PROJECT_ROOT)
+dirname = os.path.dirname(__file__)
+
+open("sample.json", "w") as outfile:
 app = Flask(__name__)  #flask constructor
 
 # A decorator used to tell the application
@@ -14,6 +20,11 @@ def Run_Simulation():  # process functions
        Number_of_Trains=request.form.get("Number_of_Trains/Carriers")
        Transport_Types=request.form.get("Switchers")
        return "The input information is, " + "Number_of_Trains: " + Number_of_Trains +"; Number_of_Persons: "+Number_of_Persons+";  Transport_Types : "+Transport_Types
+
+       '''
+       new_model_file = open(os.path.join(dirname, '../assets/model.json'), mode="w", encoding="utf-8")
+        '''
+       #create json file
     return render_template("index.html")
 
 if __name__=='__main__':
