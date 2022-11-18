41d2e3f22c470d02cd1153fd9f3ce674433f57df
Gene Chen <ceschen27@gmail.com>
2022-11-18 11:00:53 +0100

    to main

--- train_simulation/Central_Communication.py
+++ train_simulation/Central_Communication.py
@@ -7,19 +7,16 @@ def main():
     tickLength = 60
 
     sim = Simulation()
-
     # for line in sim.lines.keys():
     #     for train in sim.trains.values():
     #         if train._line == line:
     #             print(train._line)
 
-    for i in range(240):
-        sim.tickPersonGeneration(weight,tickLength)
-        sim.printAllTrainInformation()
-        print()
-        sim.tickTrain(tickLength)
-        sim.printAllTrainInformation()
-        print()
+    # for i in range(240):
+    #     sim.tickPersonGeneration(weight,tickLength)
+    #     print()
+    #     sim.tickTrain(tickLength)
+    #     print()
     # totalPassengerArrived = 0
     # totalTravelTime = 0
     # travelTimePerLine = 0
@@ -32,11 +29,11 @@ def main():
     # print(f"It took them a total of {totalTravelTime} minutes, that is an average of {totalTravelTime/totalPassengerArrived} per passenger")
     # print(f"Simulation ran for {cumulativeTick/60} minutes")
 
-    # if (animation):
-    #     sim.run_simulation_with_animation(100, 40)
-    # else:
-    #     sim.run_simulation(100, 30)
-    # return
+    if (animation):
+        sim.run_simulation_with_animation(100, 40)
+    else:
+        sim.run_simulation(100, 30)
+    return
 
 if __name__ == "__main__":
     main()
