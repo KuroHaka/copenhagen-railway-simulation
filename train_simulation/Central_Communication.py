a8ee052e198ef93c95d68a15deea36f6d1450474
Jakob Holm <holmness@Holm>
2022-11-22 19:49:39 +0100

    Made our graph undirected again

--- train_simulation/Central_Communication.py
+++ train_simulation/Central_Communication.py
@@ -4,6 +4,8 @@ import sys
 from Person import Person
 from datetime import datetime, timedelta
 
+from Algorithms import Algorithms
+
 def main():
     
     Person.create_passengers(["Lyngby","København H"], {"start": datetime.now(), "end": datetime.now()+timedelta(hours=9)}, 100)
@@ -13,11 +15,16 @@ def main():
 
     sim = Simulation()
 
-    if (animation):
-        sim.run_simulation_with_animation(100, 40)
-    else:
-        sim.run_simulation(100, 30)
-    return
+    algo = Algorithms(sim.connections,sim.stations,sim.lines)
+
+    print(algo.get_path('Lyngby','Malmparken').nodes)
+    print(algo.get_path_trains('Lyngby','Malmparken'))
+
+    # if (animation):
+    #     sim.run_simulation_with_animation(100, 40)
+    # else:
+    #     sim.run_simulation(100, 30)
+    # return
 
 if __name__ == "__main__":
     main()
